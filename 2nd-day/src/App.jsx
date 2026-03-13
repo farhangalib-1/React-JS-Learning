336f253d225435a092a248d13107c091cd6a7f5a
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-03-13 22:58:15 +0600

    rendering map with objet

--- 2nd-day/src/App.jsx
+++ 2nd-day/src/App.jsx
@@ -1,14 +1,19 @@
 function App(){
-  const users = ['Farhan', 'Ishraq', 'Galib', 'Sakib', 'Tamim']
-  return(
-    <>
-      <ul>
-        {users.map((el, index)=>
-        <li key={index} >{el}</li>
-        )}
-      </ul>
-    </>
-  )
+  const users = [
+  { id: 1, name: "Galib", age: 19 },
+  { id: 2, name: "Arnob", age: 20 },
+  { id: 3, name: "Nafi", age: 18 }
+];
+    return(
+      <>
+      {users.map(el=>(
+        <div key={el.id}>
+          <h1>Name: {el.name}</h1>
+          <h2>Age: {el.age}</h2>
+        </div>
+      ))}
+      </>
+    )
 }
 
 export default App
