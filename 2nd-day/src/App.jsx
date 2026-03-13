8951be7618e206cdfd80ce5531248ac79c0888d1
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-03-13 20:17:28 +0600

    2nd day learning. and at first I learn how to add styles on elements

---
+++ 2nd-day/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+   <>
+   <h1 style={{textAlign:"center"}}>Hello World</h1>
+   <h3 style={{textAlign:"center"}}>Farhan Galib</h3>
+   </>
+  )
+}
+
+export default App
