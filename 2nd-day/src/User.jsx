3c1edf1fe280442ad8d4306e57dce9db9b269e6d
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-03-13 20:44:51 +0600

    Destructuring added

--- 2nd-day/src/User.jsx
+++ 2nd-day/src/User.jsx
@@ -1,9 +1,9 @@
-function User(props){
+function User({name, age, country}){
     return(
     <div style={{border:"1px solid black", borderRadius:"16px", marginBottom:"20px"}}>
-    <h1 style={{textAlign:"center"}}>Name: {props.name} </h1>
-    <h1 style={{textAlign:"center"}}>Age: {props.age}</h1>
-    <h1 style={{textAlign:"center"}}>Country: {props.country} </h1>
+    <h1 style={{textAlign:"center"}}>Name: {name} </h1>
+    <h1 style={{textAlign:"center"}}>Age: {age}</h1>
+    <h1 style={{textAlign:"center"}}>Country: {country} </h1>
    </div>
     )
 }
